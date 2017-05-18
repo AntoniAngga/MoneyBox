297f7fbad0bdd2c975de4d41f3f2098e6781deb2
Antoni Angga <antoniangga14@gmail.com>
2017-05-18 23:52:11 +0700

    Transaction untuk kerangkanya sudah, sisa ada masalah di session dan perlu login dulu baru bisa masuk

--- routes/transaction.js
+++ routes/transaction.js
@@ -2,19 +2,48 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models');
 
-render.get('/',function(req,res){
-  //index untuk Transaction nya;
+router.get('/',function(req,res){
+  //index untuk Transaction nya
+  db.Category.findAll()
+  .then(Category => {
+    res.render('transaction', {Categorys:Category});
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 })
 
-render.post('/addData', function(req,res){
+router.post('/addData', function(req,res){
   //untuk add Data
+    let amount = req.body.amount;
+    let keterangan = req.body.keterangan;
+    let tanggal_transaksi = req.body.tanggal_transaksi;
+    let Category = req.body.CategoryDiPilih;
+    let currentUser = req.session.user;
+    console.log(amount);
+    console.log(tanggal_transaksi);
+    console.log(Category);
+    console.log(currentUser.id);
+    // db.transaction.create({
+    //    'amount' : amount,
+    //    'keterangan' : keterangan,
+    //    'tanggal_transaksi' : tanggal_transaksi,
+    //    'id_category' : Category,
+    //    'id_user' : currentUser.id
+    //  })
+    //  .then(()=>{
+    //    res.redirect('/transaction');
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // })
 })
 
-render.get('/edit', function(req,res){
+router.get('/edit', function(req,res){
 
 })
 
-render.get('/delete', function(req,res) {
+router.get('/delete', function(req,res) {
 
 })
 
